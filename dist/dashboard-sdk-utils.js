975f18013cba7c42c97e8c2c4ddd02e9052a7664
Alex Ross <alex@enplug.com>
2015-08-06 23:05:33 -0700

    v1.1.12 - Add smart table custom pagination template

--- dist/dashboard-sdk-utils.js
+++ dist/dashboard-sdk-utils.js
@@ -1,5 +1,17 @@
 angular.module('enplug.sdk.utils', ['enplug.sdk.utils.templates']);
 
+angular.module('enplug.sdk.utils').run(function ($templateCache) {
+
+    // Update default pagination template for Smart Table to use our directive and icons
+    $templateCache.put('template/smart-table/pagination.html',
+        '<div ng-if="pages.length >= 2">' +
+        '<st-summary></st-summary>' +
+        '<ul class="pagination plain">' +
+        '<li><a class="ion-chevron-left" ng-click="selectPage(currentPage - 1)" ng-class="{ disabled: currentPage == 1 }"></a></li>'+
+        '<li><a class="ion-chevron-right" ng-click="selectPage(currentPage + 1)" ng-class="{ disabled: currentPage == numPages }"></a></li>' +
+        '</ul></div>');
+});
+
 angular.module('enplug.sdk.utils').constant('countries', [
         {name: "UNITED STATES", code: "US"},
         {name: "AFGHANISTAN", code: "AF"},
