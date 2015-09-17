0ef989649d81677827ca82bee5a0ef4bd7fe983e
Alex Ross <alex@enplug.com>
2015-09-17 15:52:23 -0600

    Update location-aware to work with ng-href

--- dist/dashboard-sdk-utils.js
+++ dist/dashboard-sdk-utils.js
@@ -1329,39 +1329,44 @@ angular.module('enplug.sdk.utils').directive('loading', [function() {
  * @module enplug.sdk.utils.directives
  *
  */
-angular.module('enplug.sdk.utils').directive('locationAware', ['$location',
-    function ($location) {
-        return {
-            restrict: 'A',
-            replace: false,
-            link: function ($scope, $element, $attrs) {
+angular.module('enplug.sdk.utils').directive('locationAware', function ($location) {
+    'use strict';
 
-                var href = $attrs.href || $attrs.locationAware;
+    return {
+        restrict: 'A',
+        replace: false,
+        link: function (scope, element, attrs) {
 
-                function checkActive() {
-                    if (!href) {
-                        // search for a child link
-                        var anchor = $element.find('a');
-                        if (anchor) {
-                            href = anchor.attr('href');
-                        }
-                    }
-                    if (href === $location.path()) {
-                        $element.addClass('active');
-                    } else {
-                        $element.removeClass('active');
+            function getPath() {
+                var path = attrs.href || attrs.ngHref || attrs.locationAware;
+                if (!path) {
+
+                    // search for a child link
+                    var anchor = element.find('a');
+                    if (anchor) {
+                        path = anchor.attr('href') || anchor.attr('ngHref');
                     }
                 }
-
-                checkActive();
-
-                // We need to update active class every time route changes
-                $scope.$on('$routeChangeSuccess', checkActive);
-
+                return path;
             }
-        };
-    }
-]);
+
+            function checkActive() {
+                var path = getPath();
+                if (path === $location.path()) {
+                    element.addClass('active');
+                } else {
+                    element.removeClass('active');
+                }
+            }
+
+            checkActive();
+
+            // We need to update active class every time route changes
+            scope.$on('$routeChangeSuccess', checkActive);
+
+        }
+    };
+});
 
 /**
  * @ngdoc directive
