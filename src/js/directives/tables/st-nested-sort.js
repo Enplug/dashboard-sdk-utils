256c4a34b23bf52160639376f8d7c4f3231effa9
Alex Ross <alex@enplug.com>
2015-10-23 13:40:48 -0700

    v1.14.1 - Removed lodash and custom-properties dep from st-sort

--- src/js/directives/tables/st-nested-sort.js
+++ src/js/directives/tables/st-nested-sort.js
@@ -1,14 +1,28 @@
 angular.module('enplug.sdk.utils').filter('stNestedSort', [function() {
 
-    // Todo remove lodash and custom-properties dependency
+    // Returns nested property value: nested.property
+    function getter(property, item) {
+        var properties = property.split('.');
+        return properties.reduce(function (map, key) {
+            return map[key];
+        }, item);
+    }
 
-    return function(input, filter, descending) {
-        if (_.isArray(input)) {
-            if (descending) {
-                filter = '-' + filter;
-            }
-            // Sortby sorts in place, so we need to clone for when sorting is removed
-            return _.clone(input).sortBy(filter);
+    return function(input, property, descending) {
+        if (Array.isArray(input)) {
+
+            // Sort sorts in place, so we need to clone for when sorting is removed
+            return input.concat().sort(function (a, b) {
+                var sortOrder = 1;
+                if (descending) {
+                    sortOrder = -1;
+                }
+
+                var aVal = getter(property, a),
+                    bVal = getter(property, b),
+                    result = (aVal < bVal) ? -1 : (aVal > bVal) ? 1 : 0;
+                return result * sortOrder;
+            });
         }
         return input;
     };
