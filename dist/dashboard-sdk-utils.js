45c0fb50bd86098394846d2e6cddc495a784b9d0
Alex Ross <alex@enplug.com>
2015-11-16 22:46:47 -0800

    v1.15.1 - protip isolate scope

--- dist/dashboard-sdk-utils.js
+++ dist/dashboard-sdk-utils.js
@@ -1404,21 +1404,22 @@ angular.module('enplug.sdk.utils').directive('proTip', function ($log, ProTips) 
     return {
         restrict: 'E',
         replace: true,
+        scope: true,
         templateUrl: 'sdk-utils/protip.tpl',
-        link: function ($scope, $element, $attrs) {
-            // Todo take link position in protip config into account
+        link: function (scope, element, attrs) {
+
             // Get the protip config from ProTip constant
-            var path = $attrs.tip,
+            var path = attrs.tip,
                 config = _.get(ProTips, path);
             if (_.isString(config)) {
-                $scope.config = {
+                scope.config = {
                     tip: config
                 };
             } else if (_.isObject(config)) {
-                $scope.config = config;
+                scope.config = config;
             } else {
                 $log.error('Did not find pro tip config for path:', path);
-                $element.remove();
+                element.remove();
             }
         }
     };
