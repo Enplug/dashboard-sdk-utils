909b83f8803c81420ef27394f36e2e30b15645ae
Alex Ross <alex@enplug.com>
2015-12-03 16:20:44 -0800

    v1.18.0 - Update material-input to support input passthrough and fix type=number bug

--- src/js/directives/forms/material-input.js
+++ src/js/directives/forms/material-input.js
@@ -9,15 +9,13 @@
  * @param directives {Object} key:value of directives to assign to input. This directive creates isolate+transcluded scope, so remember to use $parent in values.
  * @param required {boolean},
  * @param autofocus {boolean} - causes this input to be focused after creation
+ *
+ * For better ways to do a lot of this, look to: https://github.com/angular/material/blob/master/src/components/input/input.js
+ *
  */
-angular.module('enplug.sdk.utils').directive('materialInput', ['$log', '$compile', 'GUID', '$parse',
-    function ($log, $compile, GUID, $parse) {
+angular.module('enplug.sdk.utils').directive('materialInput', ['$log', '$compile', '$parse', '$document', 'GUID',
+    function ($log, $compile, $parse, $document, GUID) {
         'use strict';
-
-        // Todo remove Lodash dependency
-        // Todo help block attribute
-        // Todo fix partial reference
-        // FIXME: label id/for probably isn't necessary because input is wrapped in label
 
         return {
             restrict: 'E',
@@ -30,7 +28,7 @@ angular.module('enplug.sdk.utils').directive('materialInput', ['$log', '$compile
             link: function ($scope, $element, $attrs, $form, $transclude) {
 
                 var ignoreAttributes = ['class', 'label', 'directives', 'field', 'focus', 'ng-if', 'ng-show', 'ng-hide'],
-                    input = $element.find('input')[0],
+                    input = $document[0].createElement('input'),
                     directives = $parse($attrs.directives)($scope),
                     autofocus = typeof $attrs.autofocus !== 'undefined',
                     id = $attrs.field + '-' + GUID.new();
@@ -54,25 +52,75 @@ angular.module('enplug.sdk.utils').directive('materialInput', ['$log', '$compile
                     }
                 });
 
+                // Type may have been set by copying attributes, if not default to text
+                if (!input.getAttribute('type')) {
+                    input.setAttribute('type', 'text');
+                }
+
+                // Set fields that used to be in HTML
+                input.setAttribute('class', 'form-control');
+                input.setAttribute('ng-model', 'model');
+                input.setAttribute('ng-class', '{ active: model }');
+                input.setAttribute('ng-model-options', '{ allowInvalid: true, debounce: 100 }');
+                input.setAttribute('id', $scope.id);
+
                 // Copy custom directives
                 angular.forEach(directives, function (value, directive) {
                     input.setAttribute(directive, value);
                 });
+
                 $element.removeAttr('directives');
 
                 // Copy any wrapped html into input template
                 $transclude(function(clone) {
-                    $element.append(clone);
-                });
 
-                // Compile input, causing AngularJS to reprocess HTML after all changes we've made, directives applied etc
-                $compile(input)($scope);
+                    var hasInput = false;
+                    if (clone) {
+
+                        // Check to see if our transcluded HTML includes an input
+                        var newInput;
+                        for (var i = 0; i < clone.length; i++) {
+                            if (clone[i].nodeName.toLowerCase() == 'input') {
+                                hasInput = true;
+                                newInput = clone[i];
+                            }
+                        }
+
+                        if (hasInput) {
+
+                            // Add default attributes to the transcluded input
+                            newInput.setAttribute('class', 'form-control');
+                            newInput.setAttribute('id', $scope.id);
+                            input.setAttribute('name', $scope.id);
+
+                            // Connect this input's model and scope to our directive's scope
+                            // FIXME: Known bug when transcluded input is number type, and has an existing model value
+                            newInput.setAttribute('ng-class', '{ active: model }');
+                            newInput.setAttribute('ng-model', 'model');
+                            $compile(clone)($scope);
+
+                            // Add input and any other included HTML to beginning of input
+                            // This case ignores all the work we did on the input above. Messy, needs refactoring.
+                            $element.prepend(clone);
+                        } else {
+                            $element.append(clone);
+                        }
+                    }
+
+                    if (!hasInput) {
+
+                        // Add our manufactured input
+                        $compile(input)($scope);
+                        $element.prepend(input);
+                    }
+                });
 
                 if (autofocus) {
                     input.focus();
                 }
 
                 $scope.formField = $form[id]; // used for ng-messages. Must retrieve after re-$compiling input
+
                 // Stupid hack to get $dirty state to be correctly set, which bizarrely doesn't happen. WTF
                 $scope.$watch('model', function (val) {
                     if (val !== undefined && val !== null && $scope.formField) {
