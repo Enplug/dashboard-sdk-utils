668f46cebc79c09594628d57e239a81d58ee09fc
mikatalk <mika.indie@gmail.com>
2016-08-15 16:12:30 -0700

    Fix Display Count directive

--- dist/dashboard-sdk-utils.js
+++ dist/dashboard-sdk-utils.js
@@ -662,29 +662,37 @@ angular.module('enplug.sdk.utils').directive('alert', function () {
  */
 angular.module('enplug.sdk.utils').directive('displayCounter', function () {
     return {
+        
         restrict: 'E',
+        
         replace: true,
+        
         scope: {
-            count: '=?',
-            muteDanger: '=?',
+            count: '=',
+            muteDanger: '=',
             vertical: '=',
-            disabled: '=?'
+            disabled: '='
         },
+        
         templateUrl: 'sdk-utils/display-counter.tpl',
+
         link: function (scope, element, attrs) {
 
-            scope.muteDanger = 'muteDanger' in attrs;
+            scope.muteDanger = 'muteDanger' in attrs && scope.muteDanger == true;
+
             scope.checkDanger = function () {
                 if ( !scope.muteDanger && scope.count <= 0 ) {
                     element.addClass('danger');
+                } else {
+                    element.removeClass('danger');
                 }
             };
             scope.checkDanger();
 
-            if ( 'vertical' in attrs && attrs.vertical === true ) {
+            if ( 'vertical' in scope && scope.vertical === true ) {
                 element.addClass('vertical');
             }
-            if ( 'disabled' in attrs ) {
+            if ( 'disabled' in scope && scope.disabled === true ) {
                 element.addClass('disabled');
             }
 
