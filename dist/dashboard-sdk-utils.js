8b9f161c12cda02b6ab7c08de8e0b5ec75ba3ffd
Becky Beauchamp <beckybeauchamp1@gmail.com>
2017-01-01 18:55:42 -0500

    Add gettextCatalog to help block directive to test

--- dist/dashboard-sdk-utils.js
+++ dist/dashboard-sdk-utils.js
@@ -1793,13 +1793,22 @@ angular.module('enplug.sdk.utils').directive('materialSwitch', function () {
     }
 });
 
-angular.module('enplug.sdk.utils').directive('helpBlock', function () {
+angular.module('enplug.sdk.utils').directive('helpBlock', ['gettextCatalog', function (gettextCatalog) {
     'use strict';
     return {
         templateUrl: 'sdk-utils/help-block.tpl',
-        restrict: 'E'
+        restrict: 'E',
+        link: function(scope) {
+            // gettextCatalog.setCurrentLanguage(navigator.browserLanguage || navigator.language);
+            gettextCatalog.setCurrentLanguage('es');
+            console.log('coming from SDKKKKKKKKK UTILS')
+            gettextCatalog.loadRemote("../dist/i18n/translations.json");
+            // When set to true, the untranslated strings will be displayed with [MISSING]
+            // next to them.
+            gettextCatalog.debug = true;
+        }
     }
-});
+}]);
 
 angular.module('enplug.sdk.utils').directive('layoutToggle', ['$rootScope', function ($rootScope) {
     'use strict';
--- src/js/directives/help-block.js
+++ src/js/directives/help-block.js
@@ -1,7 +1,16 @@
-angular.module('enplug.sdk.utils').directive('helpBlock', function () {
+angular.module('enplug.sdk.utils').directive('helpBlock', function (gettextCatalog) {
     'use strict';
     return {
         templateUrl: 'sdk-utils/help-block.tpl',
-        restrict: 'E'
+        restrict: 'E',
+        link: function(scope) {
+            // gettextCatalog.setCurrentLanguage(navigator.browserLanguage || navigator.language);
+            gettextCatalog.setCurrentLanguage('es');
+            console.log('coming from SDKKKKKKKKK UTILS')
+            gettextCatalog.loadRemote("../dist/i18n/translations.json");
+            // When set to true, the untranslated strings will be displayed with [MISSING]
+            // next to them.
+            gettextCatalog.debug = true;
+        }
     }
 });
