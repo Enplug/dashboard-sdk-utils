fa35b18f53ac88d222ed47f166662f7b7dab6eb4
Becky Beauchamp <beckybeauchamp1@gmail.com>
2017-01-06 16:47:19 -0800

    Refactor script changes for package.json to load through js versus .json file for library

--- dist/dashboard-sdk-utils.js
+++ dist/dashboard-sdk-utils.js
@@ -20,6 +20,12 @@ angular.module('enplug.sdk.utils').run(['$templateCache', 'gettextCatalog', func
 
 }]);
 
+angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
+/* jshint -W100 */
+    gettextCatalog.setStrings('es', {"A:":"A:","Add tags":"Agregar etiquetas","B:":"B:","Contain":"Contiene","Enplug Help Center":"Centro apoyo de Enplug ","G:":"G:","Need help? Go to the":"¿Necesitas ayuda? Ir a la ","Passwords must match.":"No se encontraron coincidencias.","Please enter a valid URL starting with http:// or https://":"Ingresa una URL válida empezando por http: // o https: //","Please enter a valid email address.":"Ingresa una URL válida empezando.","Position":"Posición","ProTip:":"ProTip:","R:":"R:","Something went wrong, please try again.":"Algo salió mal. Por favor, vuelva a intentarlo.","There was an error.":"Hubo un error.","This is required.":"Esto es necesario.","Upload Image":"Cargar imagen"});
+/* jshint +W100 */
+}]);
+
 angular.module('enplug.sdk.utils').constant('countries', [
     { name: 'United States', code: 'US' },
     { name: 'Afghanistan', code: 'AF' },
