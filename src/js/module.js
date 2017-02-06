f10240fa3311bc584b48064784c995dd13b86499
Michal Drewniak <drewniak@gmail.com>
2017-02-06 15:54:24 +0100

    Debugging (+13 squashed commit)
    Squashed commit:
    
    [d03853c] Debugging
    
    [ef8f0d2] Debugging
    
    [5a79237] Debugging
    
    [bf6bdbf] Debugging
    
    [5a850ec] Debugging
    
    [8390568] Debugging
    
    [ab0e1bd] Debugging
    
    [a24e8a7] Debugging
    
    [db20b0c] Debugging
    
    [b50b5eb] Debug
    
    [e72ea21] Debugging
    
    [e70cc84] Testing peerdeps
    
    [a2fd63c] Updated version number

--- src/js/module.js
+++ src/js/module.js
@@ -1,11 +1,9 @@
-angular.module('enplug.sdk.utils', ['enplug.sdk.utils.templates', 'gettext']);
+angular.module('enplug.sdk.utils', [
+	'gettext',
+	'enplug.sdk.utils.templates'
+]);
 
-angular.module('enplug.sdk.utils').run(function ($rootScope, $templateCache, gettextCatalog) {
-
-    // gettextCatalog.setCurrentLanguage(navigator.browserLanguage || navigator.language);
-    // gettextCatalog.debug = false;
-
-    // Ng class that fixes broken formatting on default text. If currentLanguage different than default, value is set to false.
+angular.module('enplug.sdk.utils').run(function ($templateCache) {
 
     // Update default pagination template for Smart Table to use our directive and icons
     $templateCache.put('template/smart-table/pagination.html',
@@ -15,5 +13,4 @@ angular.module('enplug.sdk.utils').run(function ($rootScope, $templateCache, get
         '<li><a class="ion-chevron-left" ng-click="selectPage(currentPage - 1)" ng-class="{ disabled: currentPage == 1 }"></a></li>'+
         '<li><a class="ion-chevron-right" ng-click="selectPage(currentPage + 1)" ng-class="{ disabled: currentPage == numPages }"></a></li>' +
         '</ul></div>');
-
 });
