1f9c7626dc7237b2d1ce75d78485698fcc056145
Becky Beauchamp <beckybeauchamp1@gmail.com>
2017-02-23 11:04:20 -0500

    Refactor string

--- src/js/directives/tables/st-summary.js
+++ src/js/directives/tables/st-summary.js
@@ -7,12 +7,16 @@ angular.module('enplug.sdk.utils').directive('stSummary', [function () {
     return {
         restrict: 'E',
         require: '^stTable',
-        template: '<div class="summary">{{ paginationLabel | translate }}</div>',
+        template: '<div class="summary">{{ paginationLabel | translate }} + {{stRange.from}}-{{stRange.to}} +  {{ paginationLabel2 | translate }} +  {{ size }}</div>',
         link: function ($scope, $element, $attrs, $stTable) {
 
             $scope.paginationLabel;
 
             console.log('Get text catalog: ', gettext)
+
+            $scope.paginationLabel = gettext("Showing");
+
+            $scope.paginationLabel2 = gettext("of");
 
             // Watch for updates to data
             $scope.$watch($stTable.getFilteredCollection, function  (val) {
@@ -28,8 +32,6 @@ angular.module('enplug.sdk.utils').directive('stSummary', [function () {
 
                 $scope.stRange.from = $stTable.tableState().pagination.start + 1;
                 $scope.stRange.to = $scope.currentPage === $scope.numPages ? $scope.size : ($scope.stRange.from + $scope.stItemsByPage - 1);
-
-                $scope.paginationLabel = gettext("Showing") + $scope.stRange.from + "-" + $scope.stRange.to + gettext("of") +  $scope.size;
             });
         }
     };
